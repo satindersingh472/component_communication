ac08b290b162d16dc5a5f65a7d9fa8c0327cc157
satinder <satindersingh472@gmail.com>
2022-08-13 11:59:01 -0600

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <small-user-profile v-for="(user,index) in users" :key="index" :many_users="user"></small-user-profile>
+    <small-user-profile v-for="user in users" :key="user[`id`]" :many_users="user"></small-user-profile>
   </div>
 </template>
 
@@ -18,19 +18,20 @@ export default {
           name: `name_one`,
           age: 7,
           is_premium: false,
+          id: 101
           
         },
         {
           name: `name_two`,
           age: 10,
           is_premium: true,
-          
+          id:201
         },
         {
           name: `name_three`,
           age: 9,
           is_premium: false,
-          
+          id:301
         }
       ]
     };
